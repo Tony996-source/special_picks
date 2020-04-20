68c4637e20c99bd4f41f2a092fb75b439da9a5c1
Anthony cooper <anthonyc968@gmail.com>
2020-04-20 16:17:46 +0100

    update

--- init.lua
+++ init.lua
@@ -14,6 +14,30 @@ end
 
 local diamond_capabs = minetest.registered_tools["default:pick_diamond"].tool_capabilities
 
+-- instamine pick
+
+minetest.register_tool("special_picks:instamine_pick", {
+	description = ("Instamine Pick"),
+	inventory_image = "special_picks_instamine_pick.png",
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=3,
+		groupcaps={
+			cracky = {times={[1]=0, [2]=0, [3]=0}, uses=30, maxlevel=3},
+		},
+		damage_groups = {fleshy=5},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
+
+minetest.register_craft({
+	output = "special_picks:instamine_pick",
+	recipe = {
+		{"special_picks:pointed_diamond", "special_picks:pointed_diamond", "special_picks:pointed_diamond"},
+		{"", "group:stick", ""},
+		{"", "group:stick", ""},
+	}
+})
 
 --silk touch pick
 
@@ -91,7 +115,6 @@ minetest.register_craft({
 	}
 })
 
-
 --fortune pick
 
 minetest.register_tool("special_picks:fortune_pick", {
@@ -100,7 +123,7 @@ minetest.register_tool("special_picks:fortune_pick", {
 	tool_capabilities = diamond_capabs,
 })
 
-local allowed_nodes = {"technic:mineral_sulfur","technic:mineral_lead","technic:mineral_zinc","technic:mineral_chromium","default:mineral_silver","default:mineral_tin","default:mineral_copper", "moreores:mineral_silver","moreores:mineral_mithril","moreores:mineral_tin","default:stone_with_coal","default:stone_with_iron","default:stone_with_copper","default:stone_with_mese", "default:stone_with_gold","default:stone_with_diamond"}
+local allowed_nodes = {"technic:mineral_sulfur","technic:mineral_lead","technic:mineral_zinc","technic:mineral_chromium","default:mineral_silver","default:mineral_tin","default:mineral_copper", "moreores:mineral_silver","moreores:mineral_mithril","moreores:mineral_tin","default:stone_with_coal","default:stone_with_iron","default:stone_with_copper","default:stone_with_mese", "default:stone_with_gold","default:stone_with_diamond","technic:mineral_chromium","technic:mineral_zinc","technic:mineral_lead","technic:mineral_sulfur"}
 
 add_tool("special_picks:fortune_pick", function(digger, oldnode)
 	local nam = oldnode.name
@@ -127,30 +150,11 @@ minetest.register_craftitem("special_picks:fortune_diamond", {
 minetest.register_craft({
 	output = "special_picks:fortune_diamond",
 	recipe = {
-		{"default:goldblock", "default:mese", "default:goldblock"},
-		{"default:copperblock", "default:diamond", "default:copperblock"},
-		{"default:steelblock", "default:mese", "default:steelblock"},
+		{"moreores:silver_ingot", "moreores:mithril_ingot", "moreores:silver_ingot"},
+		{"default:tin_ingot", "default:diamond", "default:tin_ingot"},
+		{"technic:wrought_iron_ingot", "moreores:mithril_ingot", "technic:wrought_iron_ingot"},
 	}
 })
-
-minetest.register_craft({
-	output = "special_picks:fortune_diamond",
-	recipe = {
-		{"moreores:silver_block", "moreores:mithril_block", "moreores:silver_block"},
-		{"moreores:tin_block", "default:diamond", "moreores:tin_block"},
-		{"default:steelblock", "moreores:mithril_block", "default:steelblock"},
-	}
-})
-
-minetest.register_craft({
-	output = "special_picks:fortune_diamond",
-	recipe = {
-		{"default:silverblock", "default:mese", "default:silverblock"},
-		{"default:tinblock", "default:diamond", "default:tinblock"},
-		{"default:copperblock", "default:mese", "default:copperblock"},
-	}
-})
-
 
 minetest.register_craft({
 	output = "special_picks:fortune_pick",
@@ -159,39 +163,6 @@ minetest.register_craft({
 		{"", "group:stick", ""},
 		{"", "group:stick", ""},
 	}
-})
-
-
---big diamond pick
-
-local capabs = table.copy(diamond_capabs)
-capabs.groupcaps.cracky.uses = capabs.groupcaps.cracky.uses*9
-
-minetest.register_tool("special_picks:big_diamondpick", {
-	description = "Big Diamond Pickaxe",
-	inventory_image = "special_picks_big_diamondpick.png",
-	tool_capabilities = capabs,
-})
-
-minetest.register_craft({
-	output = "special_picks:big_diamondpick",
-	recipe = {
-		{"default:diamondblock", "default:diamondblock", "default:diamondblock"},
-		{"", "group:tree", ""},
-		{"", "group:tree", ""},
-	}
-})
-
-
---battle pick
-
-local capabs = table.copy(diamond_capabs)
-capabs.damage_groups.fleshy = capabs.damage_groups.fleshy*2
-
-minetest.register_tool("special_picks:battle_pick", {
-	description = "Battle Pickaxe",
-	inventory_image = "special_picks_battle_pick.png",
-	tool_capabilities = capabs,
 })
 
 minetest.register_craftitem("special_picks:abrasive_paper", {
@@ -221,14 +192,6 @@ minetest.register_craft({
 	}
 })
 
-minetest.register_craft({
-	output = "special_picks:battle_pick",
-	recipe = {
-		{"special_picks:pointed_diamond", "special_picks:pointed_diamond", "special_picks:pointed_diamond"},
-		{"", "group:stick", ""},
-		{"", "group:stick", ""},
-	}
-})
 
 
 --fire pick
@@ -288,116 +251,3 @@ minetest.register_craft({
 		{"", "group:stick", ""},
 	}
 })
-
-
---liquid transportation pick
-
---[[minetest.register_tool("special_picks:liquid_transportation_pick", {
-	description = "Liquid Transportation Pickaxe",
-	inventory_image = "special_picks_fire_pick.png",
-	tool_capabilities = {
-		full_punch_interval = 0.9,
-		max_drop_level=3,
-		groupcaps={
-			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=30, maxlevel=3}
-		},
-		damage_groups = {fleshy=5},
-	},
-})]]
-
-
---glass steel pick
-
-local glass_pick_capabs = table.copy(diamond_capabs)
-local cracky = glass_pick_capabs.groupcaps.cracky
-for n,i in pairs(cracky.times) do
-	cracky.times[n] = i/2
-end
-cracky.uses = 8
-cracky.maxlevel = 2
-glass_pick_capabs.damage_groups.fleshy = glass_pick_capabs.damage_groups.fleshy-1
-
-minetest.register_tool("special_picks:glass_steel_pick", {
-	description = "Glass Steel Pickaxe",
-	inventory_image = "special_picks_glass_steel_pick.png",
-	tool_capabilities = glass_pick_capabs,
-})
-
-minetest.register_craftitem("special_picks:glass_steel_ingot", {
-	description = "Glass Steel Ingot",
-	inventory_image = "special_picks_glass_steel_ingot.png",
-})
-
-minetest.register_craft({
-	output = "special_picks:glass_steel_ingot",
-	recipe = {
-		{"default:glass", "default:glass", "default:glass"},
-		{"default:glass", "default:steel_ingot", "default:glass"},
-		{"default:glass", "default:glass", "default:glass"},
-	}
-})
-
-minetest.register_craft({
-	output = "special_picks:glass_steel_pick",
-	recipe = {
-		{"special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot"},
-		{"", "group:stick", ""},
-		{"", "group:stick", ""},
-	}
-})
-
-minetest.register_node("special_picks:glass_steel_block", {
-	description = "Glass Steel Block",
-	drawtype = "glasslike",
-	tiles = {"special_picks_glass_steel_block.png"},
-	paramtype = "light",
-	sunlight_propagates = true,
-	is_ground_content = true,
-	groups = {cracky=1,level=2},
-	sounds = default.node_sound_glass_defaults(),
-})
-
-minetest.register_craft({
-	output = "special_picks:glass_steel_block",
-	recipe = {
-		{"special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot"},
-		{"special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot"},
-		{"special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot", "special_picks:glass_steel_ingot"},
-	}
-})
-
-
-glass_pick_capabs.groupcaps.cracky.uses = glass_pick_capabs.groupcaps.cracky.uses*9
-
-minetest.register_tool("special_picks:big_glass_steel_pick", {
-	description = "Big Glass Steel Pickaxe",
-	inventory_image = "special_picks_big_glass_steel_pick.png",
-	tool_capabilities = glass_pick_capabs,
-})
-
-minetest.register_craft({
-	output = "special_picks:big_glass_steel_pick",
-	recipe = {
-		{"special_picks:glass_steel_block", "special_picks:glass_steel_block", "special_picks:glass_steel_block"},
-		{"", "group:tree", ""},
-		{"", "group:tree", ""},
-	}
-})
-
---explosion pick
-
---[[minetest.register_tool("special_picks:explosion_pick", {
-	description = "Explosion Pickaxe",
-	inventory_image = "special_picks_explosion_pick.png",
-	tool_capabilities = {
-		full_punch_interval = 0.9,
-		max_drop_level=3,
-		groupcaps={
-			cracky = {times={[1]=2.0, [2]=1.0, [3]=0.50}, uses=30, maxlevel=3}
-		},
-		damage_groups = {fleshy=5},
-	},
-})]]
-
---i need help with this pick
-
